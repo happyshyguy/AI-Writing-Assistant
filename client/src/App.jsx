95c0f429fdc8c2d88acd9a2f42b4c574a5cd5a42
Aryan Gupta <1894aryangupta@gmail.com>
2025-12-24 23:53:52 +0530

    feat(client): initialize React app with Vite and Tailwind CSS
    - Added package.json with dependencies for React, React Router, Tailwind CSS, and Vite.
    - Created main application structure with App component and routing setup.
    - Implemented basic components: Home, About, Editor, and Navbar.
    - Added styles for the application using Tailwind CSS and custom CSS.
    - Included SVG assets for React and Vite logos.
    - Configured Vite for development and production builds.

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import About from './Components/About';
+import Home from './Components/Home';
+import Editor from './Components/Editor';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/write" element={<Editor />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
