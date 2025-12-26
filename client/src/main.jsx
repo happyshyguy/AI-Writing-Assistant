f39e399f106041dc6496c9ed4a371d64b7f438dc
Aryan Gupta <1894aryangupta@gmail.com>
2025-12-27 03:46:00 +0530

    : update PrivyProvider configuration to include login methods and appearance settings

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,11 +15,17 @@ root.render(
       appId="cmjkl2a6u01iakz0cj7mmz55h"
       clientId="your-app-client-id"
       config={{
+        //login methods
+        loginMethods: ['wallet', 'email','google', 'github'],
         // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
           ethereum: {
             createOnLogin: 'users-without-wallets'
           }
+        },
+        appearance: {
+          theme: 'dark',
+          accentColor: '#0c0017ff'
         }
       }}
     >
