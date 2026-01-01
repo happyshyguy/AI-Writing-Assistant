4a5ded9f8bb5a5322dc6f09feb474af2c761dc6e
Aryan Gupta <1894aryangupta@gmail.com>
2026-01-01 17:17:20 +0530

    feat: enhance Navbar component with authentication and dynamic links

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import {usePrivy} from '@privy-io/react-auth'
+import { Link } from 'react-router-dom';
 const Navbar = () => {
+  const {authenticated, login, logout, user, ready} = usePrivy();
+  if (!ready) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div>Navbar</div>
-  )
+    <nav>
+      <ul> 
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+
+
+        <li>{authenticated && <Link to="/about">About</Link>}</li>
+        {authenticated ? <div>
+          <li>
+            <button onClick={logout}>Logout</button>
+          </li>
+        </div> : <div>
+          <li><button onClick={login}>Login</button></li>
+          </div>}
+
+
+        <li><Link to="/editor">Editor</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/logout">Logout</Link></li>
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
